370a4b08ab32e4edb190ccc0af6c6d36e71cecfa
Ahnaf Zakaria <ahnafzakaria88@gmail.com>
2025-09-22 07:14:27 +0600

    idk java stuff

---
+++ Java/Typecasting.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        int num = input.nextInt();
+//        System.out.println(num);
+//        int num = (int)(67.58f);
+//        System.out.println(num);
+//        float num2 = (float)(67);
+//        byte a = 55;
+//        byte b = 100;
+//        int c = a * b;
+//        System.out.println(c);
+//        long numb = 21321842814L;
+//        int number = 'a';
+//        System.out.println(numb);
+        int a = 1;
+        while (a < 10) {
+            System.out.println(a);
+            a++;
+        }
+        for (int i = 1; i < 10; i++){
+            System.out.println(i);
+        }
+
+    }
+}
