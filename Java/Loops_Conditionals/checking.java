e2c1909c67a1bc466e3ed795732568d036354cb4
Ahnaf Zakaria <ahnafzakaria88@gmail.com>
2025-09-24 13:37:53 +0600

    Some pattern questions

---
+++ Java/Loops_Conditionals/checking.java
@@ -0,0 +1,16 @@
+package Loops_Conditionals;
+
+public class checking {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 3){
+            System.out.println(count);
+            count++;
+        }
+        int count2 = 0;
+        do {
+            System.out.println(count2);
+            count2++;
+        } while(count2 < 3);
+    }
+}
---
+++ Java/Loops_Conditionals/triangle.java
@@ -0,0 +1,16 @@
+package Loops_Conditionals;
+
+import java.util.Scanner;
+
+public class triangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
